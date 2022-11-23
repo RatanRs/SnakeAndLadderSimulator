a903fadea1918556dbd1b788aa625e0eafbe7f93
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 12:21:48 +0530

    Added:Till the player reaches winning position 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -12,34 +12,39 @@ public class SnakeAndLadder {
 	 * 
 	 * @param args
 	 */
-	 static final int START_POSITION=0;
-	    static final int NO_PLAY=0;
-	    static final int LADDER=1;
-	    static final int SNAKE=2;
-	    static int snakeLadder(){
-	        int player=START_POSITION;
-	        int diceRoll=(int)Math.floor(Math.random()*10)%6+1;
-	        int checkOption=(int)Math.floor(Math.random()*10)%3;
-	        switch(checkOption){
-	            case NO_PLAY:
-	                player=player;
-	                break;
-	            case LADDER:
-	                player=diceRoll;
-	                break;
-	            case SNAKE:
-	                player=player-diceRoll;
-	                break;
-	        }
-	        return player;
-	    }
-	    public static void main(String[] args) {
-	        int player=snakeLadder();
-	        System.out.println(player);
-
-
-	    }
-	}
-
-
+	static final int START_POSITION=0;
+    static final int NO_PLAY=0;
+    static final int LADDER=1;
+    static final int SNAKE=2;
+    static int move=0;
+    static int snakeLadder(){
+        int player=START_POSITION;
+        while(move<++move){
+            int diceRoll = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            int checkOption = (int) Math.floor(Math.random() * 10) % 3;
+            switch (checkOption) {
+                case NO_PLAY:
+                    player = player;
+                    break;
+                case LADDER:
+                    player = player+diceRoll;
+                    break;
+                case SNAKE:
+                    player = player - diceRoll;
+                    break;
+            }
+            if(player<START_POSITION){
+                player=START_POSITION;
+            }
+            if(player==100)
+                    break;
+            move++;
+        }
+        return player;
+    }
+    public static void main(String[] args) {
+        int player=snakeLadder();
+        System.out.println(player);
+    }
+}
 
