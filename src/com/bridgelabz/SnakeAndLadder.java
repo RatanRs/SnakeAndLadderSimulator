13850af8d707ded1cfd9b077790f1cb1c6d3f75c
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 11:23:26 +0530

    Added:Welcome Meassge to snake and ladder simulator

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+/***
+ * 
+ * @author Admin
+ *
+ */
+public class SnakeAndLadder {
+	/***
+	 * 
+	 * @param args
+	 */
+public static void main(String args[]) {
+	/*
+	 * Printing welcome meassge.
+	 */
+	System.out.println("Welcome to snake and ladder simulator");
+}
+}
