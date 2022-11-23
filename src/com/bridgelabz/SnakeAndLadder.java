2027ae73520178579497ab76a64a23825c7fbd84
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 11:31:57 +0530

    Added:Player1 at start position 0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 /***
  * 
  * @author Admin
@@ -9,10 +12,16 @@ public class SnakeAndLadder {
 	 * 
 	 * @param args
 	 */
+	static int startPosition=0;
 public static void main(String args[]) {
 	/*
 	 * Printing welcome meassge.
 	 */
-	System.out.println("Welcome to snake and ladder simulator");
+	System.out.println("Enter the Number of players");
+	Scanner object=new Scanner(System.in);
+	int player1=object.nextInt();
+	player1=startPosition;
+	System.out.println("player1 Starting position = "+startPosition);
+	
 }
 }
