4f8a4e086f0a1f2a53113611e33ccc000a414513
9663461085 <ratansollapure16310@gmail.com>
2022-11-23 12:10:04 +0530

    Adde:Checked options for snake and ladder

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -12,17 +12,34 @@ public class SnakeAndLadder {
 	 * 
 	 * @param args
 	 */
-	static int startPosition=0;
-	static int method1() {
-		int player1=startPosition;
-		int diceRoll=(int)Math.floor(Math.random()*10)%6+1;
-		player1=diceRoll;
-		return player1;
-	}
-	
+	 static final int START_POSITION=0;
+	    static final int NO_PLAY=0;
+	    static final int LADDER=1;
+	    static final int SNAKE=2;
+	    static int snakeLadder(){
+	        int player=START_POSITION;
+	        int diceRoll=(int)Math.floor(Math.random()*10)%6+1;
+	        int checkOption=(int)Math.floor(Math.random()*10)%3;
+	        switch(checkOption){
+	            case NO_PLAY:
+	                player=player;
+	                break;
+	            case LADDER:
+	                player=diceRoll;
+	                break;
+	            case SNAKE:
+	                player=player-diceRoll;
+	                break;
+	        }
+	        return player;
+	    }
+	    public static void main(String[] args) {
+	        int player=snakeLadder();
+	        System.out.println(player);
 
-public static void main(String args[]) {
-	int player1=method1();
-	System.out.println(player1);
-}
-}
+
+	    }
+	}
+
+
+
